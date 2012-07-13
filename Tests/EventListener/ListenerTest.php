490d7f565a9bb1749034b7fc9e728ee087496477
Adrien Brault <adrien@thefootballsocialclub.com>
2012-07-13 15:54:06 +0200

    Add composer.json and empty tests

---
+++ Tests/EventListener/ListenerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FSC\P3PBundle\Tests\EventListener;
+
+class ListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testA()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
